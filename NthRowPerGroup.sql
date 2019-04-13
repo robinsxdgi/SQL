03ae3165bb7444f3555c47379942d580a43120fd
Rockwell Hsiang <dingxiang2015@u.northwestern.edu>
2019-04-13 11:22:27 -0500

    update

--- NthRowPerGroup.sql
+++ NthRowPerGroup.sql
@@ -1,4 +1,4 @@
 SELECT 	* FROM 
-people
+(SELECT row_number() OVER(PARTITION BY quiz_points ORDER BY id_number DESC) AS id_rank, * FROM )
 
 /* We are doing for each quiz_points group(user), output the highest 5 id_number that their team is blue*/
