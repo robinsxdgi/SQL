db1d9433732f9c7649237c2b620061f794c9a4e4
Rockwell Hsiang <dingxiang2015@u.northwestern.edu>
2019-04-13 11:26:42 -0500

    update2

--- NthRowPerGroup.sql
+++ NthRowPerGroup.sql
@@ -1,4 +1,6 @@
 SELECT 	* FROM 
-(SELECT row_number() OVER(PARTITION BY quiz_points ORDER BY id_number DESC) AS id_rank, * FROM )
+(SELECT row_number() OVER(PARTITION BY quiz_points ORDER BY id_number DESC) AS id_rank, quiz_points, team, id_number FROM people
+ WHERE team == 'blue')
+WHERE id_rank == 25
 
 /* We are doing for each quiz_points group(user), output the highest 5 id_number that their team is blue*/
